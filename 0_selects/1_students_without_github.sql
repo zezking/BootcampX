effdc6941df2efbb16709049ffbd6909cc4e5c55
zezking <zhaoenze001@gmail.com>
2021-03-01 12:51:18 -0800

    create queries directories

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -1,1 +1,12 @@
 -- SELECT id, name, email, cohort_id GROUP BY cohort_id  Wrong
+SELECT
+    id,
+    name,
+    email,
+    cohort_id
+FROM
+    students
+WHERE
+    github IS NULL
+ORDER BY
+    cohort_id
