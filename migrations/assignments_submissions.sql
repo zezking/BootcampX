dd3380ff05d562be2705d8c8affab276f5662167
zezking <zhaoenze001@gmail.com>
2021-03-01 13:56:54 -0800

    add the create answer!

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,17 +1,32 @@
+-- CREATE TABLE assignments (
+--     id SERIAL PRIMARY KEY NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     content VARCHAR,
+--     day Date,
+--     chapter VARCHAR,
+--     duration Time
+-- );
+-- CREATE TABLE assignment_submission (
+--     id SERIAL PRIMARY KEY NOT NULL,
+--     assignment_id VARCHAR(255) NOT NULL,
+--     content VARCHAR,
+--     day Date,
+--     chapter VARCHAR,
+--     duration Time
+-- );
 CREATE TABLE assignments (
     id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    content VARCHAR,
-    day Date,
-    chapter VARCHAR,
-    duration Time
+    name VARCHAR(255),
+    content TEXT,
+    day INTEGER,
+    chapter INTEGER,
+    duration INTEGER
 );
 
-CREATE TABLE assignment_submission (
+CREATE TABLE assignment_submissions (
     id SERIAL PRIMARY KEY NOT NULL,
-    assignment_id VARCHAR(255) NOT NULL,
-    content VARCHAR,
-    day Date,
-    chapter VARCHAR,
-    duration Time
+    assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+    student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+    duration INTEGER,
+    submission_date DATE
 );
