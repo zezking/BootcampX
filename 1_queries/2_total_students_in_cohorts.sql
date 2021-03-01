55b6b2531133a08c2d591a6a11a3fe072c36588c
zezking <zhaoenze001@gmail.com>
2021-03-01 13:04:19 -0800

    update actual answer from toggling answers

--- 1_queries/2_total_students_in_cohorts.sql
+++ 1_queries/2_total_students_in_cohorts.sql
@@ -1,6 +1,12 @@
+-- SELECT
+--     COUNT(cohort_id)
+-- FROM
+--     students
+-- WHERE
+--     cohort_id <= 3;
 SELECT
-    COUNT(cohort_id)
+    count(id)
 FROM
     students
 WHERE
-    cohort_id <= 3;
+    cohort_id IN (1, 2, 3);
