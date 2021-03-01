256dbd1bb416de94cee0fcdfec6b1b5d31209987
zezking <zhaoenze001@gmail.com>
2021-03-01 13:00:49 -0800

    finish total students in cohort

---
+++ 1_queries/1_names_and_id_from_cohort.sql
@@ -0,0 +1,9 @@
+SELECT
+    id,
+    name
+FROM
+    students
+WHERE
+    cohort_id = 2
+ORDER BY
+    name;
---
+++ 1_queries/2_total_students_in_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT
+    COUNT(cohort_id)
+FROM
+    students
+WHERE
+    cohort_id <= 3;
