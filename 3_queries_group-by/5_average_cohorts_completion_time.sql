c2d866873c5999070868d254cbc697d3cc98edd1
zezking <zhaoenze001@gmail.com>
2021-03-01 17:08:27 -0800

    figured out what the bugs are on for my own answers

--- 3_queries_group-by/5_average_cohorts_completion_time.sql
+++ 3_queries_group-by/5_average_cohorts_completion_time.sql
@@ -1,25 +1,26 @@
--- SELECT
---     students.name AS student,
---     AVG(assignment_submissions.duration) AS average_assignment_duration
--- FROM
---     assignment_submissions
---     JOIN students ON students.id = assignment_submissions.student_id
--- WHERE
---     end_date = Null
--- GROUP BY
---     students.name
--- ORDER BY
---     AVG(assignment_submissions.duration) My answer;
--- correc answer below
 SELECT
-    students.name as student,
-    avg(assignment_submissions.duration) as average_assignment_duration
+    students.name AS student,
+    AVG(assignment_submissions.duration) AS average_assignment_duration
 FROM
-    students
-    JOIN assignment_submissions ON student_id = students.id
+    assignment_submissions
+    JOIN students ON students.id = assignment_submissions.student_id
 WHERE
-    end_date IS NULL
+    students.end_date IS NULL --My shit didn't work because I did end_date=NULL, where it should be end_date IS NULL
 GROUP BY
-    student
+    students.name
 ORDER BY
-    average_assignment_duration DESC;
+    AVG(assignment_submissions.duration) My answer;
+
+-- correc answer below
+-- SELECT
+--     students.name as student,
+--     avg(assignment_submissions.duration) as average_assignment_duration
+-- FROM
+--     students
+--     JOIN assignment_submissions ON student_id = students.id
+-- WHERE
+--     end_date IS NULL
+-- GROUP BY
+--     student
+-- ORDER BY
+--     average_assignment_duration DESC;
