de25751dd0248727b7dfcdd41bfd3c93d3a76400
zezking <zhaoenze001@gmail.com>
2021-03-02 13:47:43 -0800

    total cohort assitance duration

---
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -0,0 +1,11 @@
+SELECT
+    cohorts.name,
+    SUM(completed_at - started_at) as total_duration
+FROM
+    assistance_requests
+    JOIN students ON students.id = assistance_requests.student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY
+    cohorts.name
+ORDER BY
+    total_duration;
