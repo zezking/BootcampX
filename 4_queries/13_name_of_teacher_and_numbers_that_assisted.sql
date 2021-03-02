e8624d20a535af46679d485fb71cd6702ba560ea
zezking <zhaoenze001@gmail.com>
2021-03-02 14:50:01 -0800

    finish name of teacher and number of assists

---
+++ 4_queries/13_name_of_teacher_and_numbers_that_assisted.sql
@@ -0,0 +1,16 @@
+SELECT
+    DISTINCT teachers.name as teacher,
+    cohorts.name as cohort,
+    COUNT(assistance_requests.teacher_id) as total_assistances
+FROM
+    teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+WHERE
+    cohorts.name = 'JUL02'
+GROUP BY
+    teachers.name,
+    cohorts.name
+ORDER BY
+    teacher;
