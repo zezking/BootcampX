aaad994279f16697e7c8deb96df11068228c70fc
zezking <zhaoenze001@gmail.com>
2021-03-03 14:52:22 -0800

    finish teacher js

---
+++ teacher.js
@@ -0,0 +1,39 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "vagrant",
+  port: 5432,
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool.connect();
+
+const getCohortWithTeacherAssists = (cohortName) => {
+  pool
+    .query(
+      `SELECT
+    DISTINCT teachers.name as teacher,
+    cohorts.name as cohort
+FROM
+    teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+WHERE
+    cohorts.name = '${cohortName}'
+ORDER BY
+    teacher;
+    `
+    )
+    .then((data) => {
+      data.rows.forEach((user) => {
+        console.log(`${user.cohort}: ${user.teacher}`);
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+let cohortName = process.argv[2];
+
+getCohortWithTeacherAssists(cohortName);
